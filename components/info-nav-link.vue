70413335a21e80809eda99f2224f22ca120560d2
neopostmodern <clemens@neopostmodern.com>
2023-10-30 16:32:12 +0100

    feat: layout and navigation for about pages

---
+++ components/info-nav-link.vue
@@ -0,0 +1,34 @@
+<template>
+  <nuxt-link
+    :to="to"
+    :class="{ active: $route.fullPath === to }"
+  >
+    <img
+      :src="src"
+      alt=""
+    >
+    <slot />
+  </nuxt-link>
+</template>
+
+<script setup lang="ts">
+defineProps<{ to: string, src: string }>()
+</script>
+
+<style scoped>
+a {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: var(--padding);
+
+  text-decoration: none;
+  color: white;
+}
+.active {
+  text-decoration: underline;
+}
+img {
+  height: 2rem;
+}
+</style>
---
+++ pages/credits.vue
@@ -0,0 +1,15 @@
+<template>
+  <info-layout>
+    <p>
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+      dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita
+      kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+      sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+      voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+      sanctus est Lorem ipsum dolor sit amet.
+    </p>
+    <h1>(credits)</h1>
+  </info-layout>
+</template>
+<script setup lang="ts">
+</script>
